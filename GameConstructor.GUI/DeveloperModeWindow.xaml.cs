ce8c575c0b583163ece4962be2f6ab6187cbe4b5
Юрий Саночкин <yuisanochkin@edu.hse.ru>
2018-05-21 13:02:11 +0300

    Initial GUI

---
+++ GameConstructor.GUI/DeveloperModeWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameConstructor.GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для DeveloperModeWindow.xaml
+    /// </summary>
+    public partial class DeveloperModeWindow : Window
+    {
+        int _questionNumber = 1;
+        int _answerNumber = 1;
+
+        public DeveloperModeWindow()
+        {
+            InitializeComponent();
+
+            QuestionsListBox.ItemsSource = new List<List<List<int>>> { new List<List<int>> { new List<int> { 1 } }, new List<List<int>> { new List<int> { 2, 3, 4 }, new List<int> { 1 } }, new List<List<int>> { new List<int> { 5, 7 }, new List<int> { 6 }, new List<int> { 7, 8, 9, 10 } } };
+        }
+
+
+        private void AnswersListBox_Initialized(object sender, EventArgs e)
+        {
+            ListBox ListBox = sender as ListBox;
+            ListBox.ItemsSource = ListBox.DataContext as List<List<int>>;
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+
+            mainWindow.Show();
+        }
+
+
+        private void AnswerTextBox_Initialized(object sender, EventArgs e)
+        {
+            TextBox TextBox = sender as TextBox;
+            TextBox.Text = "Вариант ответа " + _answerNumber;
+
+            _answerNumber++;
+        }
+
+
+        private void QuestionTextBlock_Initialized(object sender, EventArgs e)
+        {
+            TextBlock TextBlock = sender as TextBlock;
+            TextBlock.Text = "Вопрос " + _questionNumber;
+
+            _questionNumber++;
+
+            _answerNumber = 1;
+        }
+
+
+        private void ReactionsListBox_Initialized(object sender, EventArgs e)
+        {
+            ListBox ListBox = sender as ListBox;
+            ListBox.ItemsSource = ListBox.DataContext as List<int>;
+        }
+
+
+        private void ChangesOfCharacteristicListBox_Initialized(object sender, EventArgs e)
+        {
+            (sender as ListBox).ItemsSource = new int[3] { 0, 0, 0 };
+        }
+    }
+}
---
+++ GameConstructor.GUI/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameConstructor.GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double _defaultWidthOfTheWindow;
+        double _defaultHeightOfTheWindow;
+        double _defaultFontSize;
+        Thickness _defaultPadding;
+
+        double _minimalChangingOfTheSizeParameters;
+
+        double _fontSize;
+        Thickness _padding;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _defaultWidthOfTheWindow = Width;
+            _defaultHeightOfTheWindow = Height;
+            _defaultPadding = PlayingModeButton.Padding;
+            _defaultFontSize = PlayingModeButton.FontSize;
+        }
+
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _minimalChangingOfTheSizeParameters = Math.Min(Height / _defaultHeightOfTheWindow, Width / _defaultWidthOfTheWindow);
+
+            _fontSize = _defaultFontSize * Math.Pow (_minimalChangingOfTheSizeParameters, 1.2);
+
+            _padding.Left = _defaultPadding.Left * Math.Pow(_minimalChangingOfTheSizeParameters, 1.8);
+            _padding.Right = _defaultPadding.Right * Math.Pow(_minimalChangingOfTheSizeParameters, 1.8);
+            _padding.Top = _defaultPadding.Top * Math.Pow(_minimalChangingOfTheSizeParameters, 1.2);
+            _padding.Bottom = _defaultPadding.Bottom * Math.Pow(_minimalChangingOfTheSizeParameters, 1.2);
+
+            PlayingModeButton.FontSize = _fontSize;
+            DeveloperModeButton.FontSize = _fontSize;
+
+            PlayingModeButton.Padding = _padding;
+            DeveloperModeButton.Padding = _padding;
+        }
+
+
+        private void DeveloperModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            DeveloperModeWindow developerModeWindow = new DeveloperModeWindow();
+
+            developerModeWindow.Show();
+
+            Close();
+        }
+    }
+}
