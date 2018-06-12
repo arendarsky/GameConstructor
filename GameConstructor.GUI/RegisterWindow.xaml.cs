4d0083869147cb5dd44486a50925b50bf799d673
Юрий Саночкин <yuisanochkin@edu.hse.ru>
2018-06-12 17:57:12 +0300

    Полностью изменен дизайн Register Window

--- GameConstructor.GUI/RegisterWindow.xaml.cs
+++ GameConstructor.GUI/RegisterWindow.xaml.cs
@@ -21,43 +21,166 @@ namespace GameConstructor.GUI
     /// </summary>
     public partial class RegisterWindow : Window
     {
+        private const string defaultLoginText = "Введите логин";
+        private const string defaultPasswordText = "Введите пароль";
+        private const string defaultRepeatPasswordText = "Повторите пароль";
+        private const string defaultEmail = "Укажите Email";
+
+
         IStorage _storage;
+        
+
         public RegisterWindow(IStorage storage)
         {
             _storage = storage;
+
             InitializeComponent();
+
+            RestartTheWindow();
         }
+
+
+        private void RestartTheWindow()
+        {
+            LoginTextBox.Text = defaultLoginText;
+            LoginTextBox.Foreground = Brushes.Gray;
+
+            PasswordTextBox.Text = defaultPasswordText;
+            PasswordTextBox.Foreground = Brushes.Gray;
+            PasswordTextBox.Visibility = Visibility.Visible;
+
+            RepeatPasswordTextBox.Text = defaultRepeatPasswordText;
+            RepeatPasswordTextBox.Foreground = Brushes.Gray;
+            RepeatPasswordTextBox.Visibility = Visibility.Visible;
+
+            EmailTextBox.Text = defaultEmail;
+            EmailTextBox.Foreground = Brushes.Gray;
+
+            PasswordBox.Password = "";
+            PasswordBox.Visibility = Visibility.Hidden;
+
+            RepeatPasswordBox.Password = "";
+            RepeatPasswordBox.Visibility = Visibility.Hidden;
+        }
+
+
+
+        private void LoginTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTextBox.Text == defaultLoginText)
+            {
+                LoginTextBox.Text = "";
+
+                LoginTextBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void LoginTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTextBox.Text == "")
+            {
+                LoginTextBox.Text = defaultLoginText;
+
+                LoginTextBox.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void PasswordTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordTextBox.Visibility = Visibility.Hidden;
+
+            PasswordBox.Visibility = Visibility.Visible;
+
+            PasswordBox.Focus();
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Password == null || PasswordBox.Password == "")
+            {
+                PasswordBox.Visibility = Visibility.Hidden;
+
+                PasswordTextBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RepeatPasswordTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            RepeatPasswordTextBox.Visibility = Visibility.Hidden;
+
+            RepeatPasswordBox.Visibility = Visibility.Visible;
+
+            RepeatPasswordBox.Focus();
+        }
+
+        private void RepeatPasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (RepeatPasswordBox.Password == null || RepeatPasswordBox.Password == "")
+            {
+                RepeatPasswordBox.Visibility = Visibility.Hidden;
+
+                RepeatPasswordTextBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void EmailTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (EmailTextBox.Text == defaultEmail)
+            {
+                EmailTextBox.Text = "";
+
+                EmailTextBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void EmailTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (EmailTextBox.Text == "")
+            {
+                EmailTextBox.Text = defaultEmail;
+
+                EmailTextBox.Foreground = Brushes.Gray;
+            }
+        }
+
+
 
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
         {
             string login = LoginTextBox.Text;
-            string password = User.GetHash(PasswordTextBox.Password);
-            string passwordConf = User.GetHash(RepeatPasswordTextBox.Password);
+            string password = User.GetHash(PasswordBox.Password);
+            string passwordConf = User.GetHash(RepeatPasswordBox.Password);
+
             if (string.IsNullOrWhiteSpace(login))
             {
                 LoginTextBox.Focus();
                 return;
             }
+
             if (_storage.Users.Items.FirstOrDefault(u => u.Login.ToLower() == login.ToLower()) != null)
             {
                 MessageBox.Show("Пользователь с таким логином уже существует!");
                 return;
             }
-            if (string.IsNullOrWhiteSpace(PasswordTextBox.Password))
+
+            if (string.IsNullOrWhiteSpace(PasswordBox.Password))
             {
                 PasswordTextBox.Focus();
                 return;
             }
-            if (string.IsNullOrWhiteSpace(RepeatPasswordTextBox.Password))
+
+            if (string.IsNullOrWhiteSpace(RepeatPasswordBox.Password))
             {
                 RepeatPasswordTextBox.Focus();
                 return;
             }
+
             if (password != passwordConf)
             {
                 MessageBox.Show("Пароли не совпадают!", "Ошибка");
                 return;
             }
+
             User NewUser = new User
             {
                 Login = login,
@@ -65,7 +188,9 @@ namespace GameConstructor.GUI
                 Email = EmailTextBox.Text,
                 Games = new List<Game>()
             };
+
             _storage.SaveUser(NewUser);
+
             DialogResult = true;
         }
     }
