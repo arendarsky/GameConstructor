b252ffacfbaf3e9c02bad83b72468b8c8d14dc73
Юрий Саночкин <yuisanochkin@edu.hse.ru>
2018-06-14 22:38:58 +0300

    Исправление багов

--- GameConstructor.GUI/ResultAfterQuestionWindow.xaml.cs
+++ GameConstructor.GUI/ResultAfterQuestionWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace GameConstructor.GUI
 
         private bool _goingToTheNextQuestionWindow = false;
         private bool _goingToTheEndOfTheGameWindow = false;
+        private bool _goingForward = false;
         bool _testingMode;
         bool _wereThereAlreadySomeChangings;
 
@@ -157,7 +158,7 @@ namespace GameConstructor.GUI
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (_goingToTheEndOfTheGameWindow)
+            if (_goingForward && _goingToTheEndOfTheGameWindow)
             {
                 EndOfGameWindow endOfGameWindow = new EndOfGameWindow(_testingMode, _storage, _game,
                     _localCharacteristics, _wereThereAlreadySomeChangings);
@@ -165,7 +166,7 @@ namespace GameConstructor.GUI
                 endOfGameWindow.Show();
             }
 
-            else if (_goingToTheNextQuestionWindow)
+            else if (_goingForward && _goingToTheNextQuestionWindow)
             {
                 QuestionsWindow questionsWindow = new QuestionsWindow(_testingMode, _storage, _game,
                     _numberOfQuestionsShown, _localCharacteristics, _wereThereAlreadySomeChangings);
@@ -183,6 +184,7 @@ namespace GameConstructor.GUI
 
                     playingModeWindow.Show();
                 }
+
                 else
                 {
                     Developer_III_Window developerWindow = new Developer_III_Window(
